c0fa756304f02c02a4da02d45200e6963ac44cb4
Susan Grasso <profgrasso11@gmail.com>
2018-12-14 13:59:24 -0500

    Create index.php

---
+++ webDesign2018.19/root_seasideCafe/index.php
@@ -0,0 +1,27 @@
+<!Doctype html>
+<html>
+<head>
+  <title> Seaside Cafe Demo 2018 </title>
+
+</head>
+
+<body>
+
+  <header>
+
+      <h1> Seaside Cafe</h1>
+      <h3> Great Food from Land and Sea </h3>
+
+      <nav>
+        <ul>
+          <li><a href="index.php"> HOME </a></li>
+          <li><a href="##"> MENU </a> </li>
+        </ul>
+
+      </nav>
+  </header>
+
+
+</body>
+
+</html>
